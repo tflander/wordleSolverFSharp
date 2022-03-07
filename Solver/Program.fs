94cafca938d85fc4b975c1012ea600b54b2be52b
todd flanders <toddfbass@gmail.com>
2022-03-07 16:05:32 -0500

    parse word file

--- Solver/Program.fs
+++ Solver/Program.fs
@@ -1,4 +1,6 @@
 open System
+open System.Collections.Generic
+open System.Text.RegularExpressions
 
 type LetterState = {
     IsInWord: bool
@@ -22,10 +24,32 @@ type Wordle(answer: string) =
  
     member __.AnalyzeLetter(letter: char) =
         {Letter = letter; State = {IsInWord = true; IsInCorrectPosition = true}}
-    
 
+let fiveLetterWordPattern = Regex("^[A-Za-z]{5}$")
+let isFiveLetterWord(word: string) =
+    fiveLetterWordPattern.IsMatch(word)
+
+let ReadFiveLetterWords(filePath: string) =
+    (IO.File.ReadAllText filePath).Split("\n")
+        |> Array.filter isFiveLetterWord
+        |> Array.map (fun word -> word.ToUpperInvariant())
+    
 [<EntryPoint>]
 let main argv =
-    printfn "Hello world"
+    printfn "Reading words file..."
+    let fiveLetterWords = ReadFiveLetterWords("words.txt")
+    printfn $"%i{fiveLetterWords.Length}"
+    
+    let letterCounts = new Dictionary<char, int>()
+    
+    let IncCharCount(c: char) =
+        letterCounts.[c] <- letterCounts.GetValueOrDefault(c, 0) + 1
+        
+    let CountLettersInWord(word: string) =
+        word.ToCharArray() |> Seq.iter IncCharCount
+    
+    fiveLetterWords |> Seq.iter CountLettersInWord
+    
+    // fiveLetterWords |> Seq.iter (fun x -> printfn $"%s{x}")
     0
     
