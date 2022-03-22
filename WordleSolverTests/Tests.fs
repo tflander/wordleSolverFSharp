a7ba0c13f4cabe335dc2aa2d349981e4690c18d5
Todd Flanders <toddfbass@gmail.com>
2022-03-22 12:47:39 -0400

    first pass at testing DSL

--- WordleSolverTests/Tests.fs
+++ WordleSolverTests/Tests.fs
@@ -15,24 +15,31 @@ type ``Evauluate guess tests`` () =
 //            [| {IsInWord = true; IsInCorrectPosition = true}; true |]
 //        ]
     
-    let ExpectResultForGuess(guess: string, expectedStates: LetterState list) =
-        let chars = guess.ToCharArray()
-        chars
-            |> Array.mapi (fun i c -> {Letter = c; State = expectedStates.[i]})
-            |> Array.toList
-                
+    let GivenGameWithSolution(solution: string) =
+        Wordle(solution)
+        
+    let WhenGuess(guess: string) (game: Wordle) =
+        game.Guess(guess)
+        
+    let ExpectResult(expectedStates: LetterState list) (actualResult: LetterAnswer list) =
+        let actualStates = List.map (fun answer -> answer.State) actualResult
+        test <@ actualStates = expectedStates @>
+
     [<Fact>]
-    member __.AllHits() =
-        let game = Wordle("MUSIC")        
-        test <@ game.Guess("MUSIC") = ExpectResultForGuess("MUSIC", [Hit; Hit; Hit; Hit; Hit]) @>
-                
+    member __.AllHits2() =
+        GivenGameWithSolution("MUSIC")
+            |> WhenGuess("MUSIC")
+            |> ExpectResult([Hit; Hit; Hit; Hit; Hit])
+                                    
     [<Fact>]
     member __.AllMisses() =
-        let game = Wordle("MUSIC")
-        test <@ game.Guess("TEXAN") = ExpectResultForGuess("TEXAN", [Miss; Miss; Miss; Miss; Miss]) @>
+        GivenGameWithSolution("MUSIC")
+            |> WhenGuess("TEXAN")
+            |> ExpectResult([Miss; Miss; Miss; Miss; Miss])
                 
     [<Fact>]
     member __.OneNearMiss() =
-        let game = Wordle("MUSIC")
-        test <@ game.Guess("TEXAS") = ExpectResultForGuess("TEXAS", [Miss; Miss; Miss; Miss; NearMiss]) @>
+        GivenGameWithSolution("MUSIC")
+            |> WhenGuess("TEXAS")
+            |> ExpectResult([Miss; Miss; Miss; Miss; NearMiss])
                 
