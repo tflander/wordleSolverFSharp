0a2e2f326d5b2a7349d9abd9bb3a144011e9d5f3
Todd Flanders <toddfbass@gmail.com>
2022-03-28 15:22:32 -0400

    break out play function for e2e tests

--- WordleSolverTests/E2eTests.fs
+++ WordleSolverTests/E2eTests.fs
@@ -13,28 +13,28 @@ type ``End To End Tests`` () =
 
     let fiveLetterWords = ReadFiveLetterWords("words.txt")
 
+    let randomWord (wordlist: string[]) =
+        let index = Random().Next(wordlist.Length)
+        wordlist.[index]
+
+    let play(game: Wordle) = 
+        let mutable wordList = fiveLetterWords
+        let mutable guessCount = 1
+        let mutable guess = randomWord wordList        
+        let mutable guessResult = game.Guess guess
+    
+        while(not (IsGameWon guessResult)) do
+            guessCount <- guessCount + 1
+            wordList <- FilterWords guessResult wordList
+            guess <- randomWord wordList
+            guessResult <- game.Guess guess
+            
+        test <@ guessCount <= 6  @>
+        
 //    [<Fact(Skip = "This is spike code to delete")>]
     [<Fact>]
     member __.``Game Simulation``() =
-        let randomWord (wordlist: string[]) =
-            let index = (new System.Random()).Next(wordlist.Length)
-            wordlist.[index]
 
-        let mutable wordList = fiveLetterWords
         let game = GivenGameWithSolution("MUSIC")
-        
-        let foo() = 
-            let mutable guessCount = 1
-            let mutable guess = randomWord wordList        
-            let mutable guessResult = game.Guess guess
-        
-            while(not (IsGameWon guessResult)) do
-                guessCount <- guessCount + 1
-                wordList <- FilterWords guessResult wordList
-                guess <- randomWord wordList
-                guessResult <- game.Guess guess
-                
-            test <@ guessCount <= 6  @>
-            
-        foo()
+        play game
 
